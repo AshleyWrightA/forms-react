a00b9bff8d97ba511ecf199ecb639bd70d030e85
AshleyWrightA <AshleyWrightAppDev@outlook.com>
2023-02-14 13:44:26 -0700

    Added Initial Layout & Media queries

--- forms-react/src/app/components/splash.jsx
+++ forms-react/src/app/components/splash.jsx
@@ -1,10 +1,11 @@
 export default function Splash() {
   return (
     <div className="splash">
+      <div className="splash__spacer"></div>
       <div className="splash__container">
         <h1 className="splash__header">Cartomancy</h1>
       </div>
-      <h2 Splash__credits>
+      <h2 className="splash__credits">
         Photo by: <a href="">Viva Luna Studios</a>
       </h2>
     </div>
